a186acb04fcdad22c0f2f3bcc55384c823cbbc16
qianglaiYJ <107804398+qianglaiYJ@users.noreply.github.com>
2023-07-21 18:44:16 +0800

    Update ReggieApplication.java

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -15,7 +15,7 @@ public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
 //       log 对象 是@Slf4j 注解所提供的，作用是在控制台输出日志信息
-        System.out.println("git代码提交");
+        
 
         log.info("项目启动成功");
     }
